a7db284910172c3518a3a53dfabc969cd9f390aa
Travis Ritch <travisritch@gmail.com>
2021-08-19 05:51:18 -0600

    Finished create_dir and build functions

--- ssg/site.py
+++ ssg/site.py
@@ -1,9 +1,18 @@
 from pathlib import Path
 
+
 class Site:
     def __init__(self, source, dest):
         self.source = Path(source)
         self.dest = Path(dest)
 
     def create_dir(self, path):
-        directory = self.dest / relative_to(self.source)
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)
