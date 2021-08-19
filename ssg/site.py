7c2e179d6bcd472577c89a7583f0f0bb1d4395d5
Travis Ritch <travisritch@gmail.com>
2021-08-19 05:40:12 -0600

    Created create_dir

--- ssg/site.py
+++ ssg/site.py
@@ -2,5 +2,8 @@ from pathlib import Path
 
 class Site:
     def __init__(self, source, dest):
-        self._path = Path(source)
-        self._dest = Path(dest)
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+    def create_dir(self, path):
+        directory = self.dest / relative_to(self.source)
