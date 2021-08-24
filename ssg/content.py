8045f95b1c7d15a2fe674b3fb026077bfa9699e9
Travis Ritch <travisritch@gmail.com>
2021-08-24 05:55:15 -0600

    Made changes to content.py

---
+++ ssg/content.py
@@ -0,0 +1,14 @@
+import re
+from yaml import load, FullLoader
+from collections.abc import Mapping
+
+class Content(Mapping):
+    __delimeter = r"^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimeter, re.MULTILINE)
+
+    @classmethod
+    def load(cls, string):
+        _, fm, content = cls.__regex.split(string, 2)
+
+    load(fm, Loader=FullLoader)
+    return cls(metadata, content)
