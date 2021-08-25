9353316f0ba635d1b1690d55db7e02f472b7a522
Travis Ritch <travisritch@gmail.com>
2021-08-25 05:46:09 -0600

    Finishing content.py

--- ssg/content.py
+++ ssg/content.py
@@ -14,4 +14,34 @@ class Content(Mapping):
 
     def __init__(self, metadata, content):
         self.data = metadata
-        self.data += {"content": content}
+        self.data["content"] = content
+
+    @property
+    def body(self):
+        return self.data["content"]
+
+    @property
+    def type(self):
+        return self.data["type"] if "type" in self.data else None
+    
+    @type.setter
+    def type(self, type):
+        self.data["type"] = type
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __iter__(self):
+        self.data.__iter__()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        data = {}
+
+        for key, value in self.data.items():
+            if key != "content":
+                data[key] = value
+
+        return str(data)
