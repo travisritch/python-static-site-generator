69aff080e7e12b07a3c19486cbfc7ca4c24925bf
Travis Ritch <travisritch@gmail.com>
2021-08-27 05:31:21 -0600

    Fixing write method calls in parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -41,7 +41,7 @@ class MarkdownParser(Parser):
     def parse(self, path: Path, source: Path, dest: Path):
         content = Content.load(self.read(path))
         html = markdown(content.body)
-        self.write(html, dest)
+        self.write(path, dest, html)
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n"
             ).format(path.name, content)
@@ -53,7 +53,7 @@ class ReStructuredTextParser(Parser):
     def parse(self, path: Path, source: Path, dest: Path):
         content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name="html5")
-        self.write(html["html_body"], dest)
+        self.write(path, dest, html["html_body"])
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n"
             ).format(path.name, content)
